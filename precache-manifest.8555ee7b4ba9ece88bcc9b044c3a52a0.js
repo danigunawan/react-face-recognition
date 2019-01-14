3f7b7417aed6f1adfaf47ba6f252302a92661848
Tanya <tanlull@gmail.com>
2019-01-14 13:16:36 +0700

    Updates

---
+++ precache-manifest.8555ee7b4ba9ece88bcc9b044c3a52a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccd20247dfe4bef3cae08bbc4eeede84",
+    "url": "/react-face-recognition/static/media/test.ccd20247.jpg"
+  },
+  {
+    "revision": "2a5d5ea8ef6a7078db61",
+    "url": "/react-face-recognition/static/js/runtime~main.2a5d5ea8.js"
+  },
+  {
+    "revision": "03d8aeb8ea25f29e3f79",
+    "url": "/react-face-recognition/static/js/main.03d8aeb8.chunk.js"
+  },
+  {
+    "revision": "b3bd9a0deb4869a93353",
+    "url": "/react-face-recognition/static/js/1.b3bd9a0d.chunk.js"
+  },
+  {
+    "revision": "03d8aeb8ea25f29e3f79",
+    "url": "/react-face-recognition/static/css/main.0a675e10.chunk.css"
+  },
+  {
+    "revision": "62288f764a283236c7a5aa9dbabec376",
+    "url": "/react-face-recognition/index.html"
+  }
+];
